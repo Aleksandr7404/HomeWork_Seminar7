0eea2b23faaf1aaf3ca7ad77ecffc9637f12ec25
Aleksandr <cross_comp@hotmail.com>
2024-01-28 01:53:10 +0300

    add HW: task3

---
+++ Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+int[] CreateArray(int size)
+{
+    int[] arrayRnd = new int[size];
+    for (int i = 0; i < size; i++)
+        arrayRnd[i] = new Random().Next(0, 99);
+    return arrayRnd;
+}
+void ShowArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+
+}
+void ShowArrayBackwards(int[] arr, int i = 0)
+{
+    if (i < arr.Length)
+    {
+        ShowArrayBackwards(arr, i + 1);
+        Console.Write($"{arr[i]} ");
+    }
+}
+Console.Clear();
+int[] array = CreateArray(5);
+ShowArray(array);
+Console.WriteLine();
+ShowArrayBackwards(array);
